76116057be53cdc46492ebbf258170fdfbb72d88
algorik5 <algorik5@gmail.com>
2020-04-02 17:03:14 +0900

    'ok'

---
+++ _git_init.bat
@@ -0,0 +1,8 @@
+echo "# util-nred-aproject" >> README.md
+git init
+git add .
+git commit -m "ok"
+git remote add origin https://github.com/algorik5/util-nred-aproject.git
+git push -u origin master
+
+pause
---
+++ _git_push.bat
@@ -0,0 +1,5 @@
+git add .
+git commit -m 'ok'
+git push -u origin master
+
+pause
